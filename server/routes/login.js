d4c39ebe29e71f9e03ba8d55b57e78698933819f
iwin2471 <iwin12372@gmail.com>
2016-10-03 23:39:52 +0900

    2ed start

---
+++ server/routes/login.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    Logined = req.session.logined;
+    if(!isLogined){
+      res.render('login', {});
+    }else{
+      res.redirect('/');
+    }
+});
+
+
+router.post('/', function(req, res, next) {
+    if (req.body.email === '' || req.body.passwd === '') {
+        res.redirect('login');
+    }
+
+    Users.findOne({
+        "email": req.body.email,
+        "passwd": req.body.passwd
+    }, function(err, member) {
+        if (member !== null) {
+            req.session.regenerate(function() {
+                req.session.logined = true;
+                Users.update({email:member.email}, {$set:{"Logined": true}}, function(err, result) {
+                  if (err);
+                });
+                req.session.nickname = member.nickname;
+                name = member.nickname;
+                console.log(name + "님 로그인하셨습니다");
+                res.redirect('/');
+            });
+        }
+    });
+});
+
+module.exports = router;
